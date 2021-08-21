ae16ad3b84ede698382f914b119c16cdc2ea157d
Benjamin Frye <ben.j.frye@gmail.com>
2021-08-21 18:54:16 +1000

    Add details page.

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from models import OCCUPATION_DESCRIPTIONS
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
-def index():
+def search():
     search_occupation = request.args.get('search_occupation')
     return render_template('search.html',
                            search_occupation=search_occupation,
-                           occupations=OCCUPATION_DESCRIPTIONS)
+                           occupations=OCCUPATION_DESCRIPTIONS.values())
+
+@app.route('/occupations/<int:code>', methods=['GET'])
+def detail(code: int):
+    occupation = OCCUPATION_DESCRIPTIONS[code]
+    return render_template('detail.html', occupation=occupation)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -3,18 +3,19 @@ from typing import NamedTuple
 import os
 import os.path
 
-
 class OccupationDescription(NamedTuple):
-    code: str
+    code: int
     title: str
     description: str
 
 
-OCCUPATION_DESCRIPTIONS: list[OccupationDescription] = []
+OCCUPATION_DESCRIPTIONS: dict[str, OccupationDescription] = dict()
+
 with open('data/occupation_descriptions.csv', encoding='utf-8-sig') \
         as csvreader:
-    reader = DictReader(csvreader)
-    OCCUPATION_DESCRIPTIONS = [
-            OccupationDescription(
-                r['ANZSCO_Code'], r['ANZSCO_Title'], r['ANZSCO_Desc'])
-            for r in reader]
+            reader = DictReader(csvreader)
+            for r in reader:
+                o = OccupationDescription(int(r['ANZSCO_Code']), r['ANZSCO_Title'], r['ANZSCO_Desc'])
+                OCCUPATION_DESCRIPTIONS[o.code] = o
+
+
