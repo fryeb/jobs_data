28e7f159722bd5adac0496dcd0024feef55423fa
Benjamin Frye <ben.j.frye@gmail.com>
2021-08-21 20:39:22 +1000

    Add technology tools.

--- app.py
+++ app.py
@@ -1,26 +1,34 @@
 #!/usr/bin/env python3
 
 from flask import Flask, request, render_template, jsonify
-from models import OCCUPATION_DESCRIPTIONS, OCCUPATION_CORE_COMPETENCIES
+from models import TITLE_TO_CODE, OCCUPATION_DESCRIPTIONS, OCCUPATION_CORE_COMPETENCIES, OCCUPATION_TECHNOLOGY_TOOLS
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def search():
-    search_occupation = request.args.get('search_occupation')
+    current_occupation = request.args.get('current_occupation')
     return render_template('search.html',
-                           search_occupation=search_occupation,
-                           occupations=OCCUPATION_DESCRIPTIONS.values())
+            current_occupation=current_occupation,
+            occupations=OCCUPATION_DESCRIPTIONS.values())
 
 @app.route('/occupations/<int:code>', methods=['GET'])
 def detail(code: int):
+    current_title = request.args.get('current_occupation')
+    print(current_title)
+    current_occupation = OCCUPATION_DESCRIPTIONS[TITLE_TO_CODE[current_title]]
+    current_tech_tools = OCCUPATION_TECHNOLOGY_TOOLS[current_occupation.code]
+
     occupation = OCCUPATION_DESCRIPTIONS[code]
     core_competencies = OCCUPATION_CORE_COMPETENCIES[code]
+    technology_tools = OCCUPATION_TECHNOLOGY_TOOLS[code]
     return render_template(
             'detail.html',
             occupation=occupation,
-            core_competencies=core_competencies)
+            core_competencies=core_competencies,
+            technology_tools=technology_tools,
+            current_tech_tools=current_tech_tools)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -20,22 +20,26 @@ class CoreCompetency(NamedTuple):
     score: int
 
 OCCUPATION_DESCRIPTIONS: dict[int, OccupationDescription] = dict()
+OCCUPATION_CORE_COMPETENCIES: dict[int, list[CoreCompetency]] = dict()
+OCCUPATION_TECHNOLOGY_TOOLS: dict[int, list[str]] = dict()
+TITLE_TO_CODE: dict[str, int] = dict()
 
 for r in csvrows('data/occupation_descriptions.csv'):
     o = OccupationDescription(int(r['ANZSCO_Code']), r['ANZSCO_Title'], r['ANZSCO_Desc'])
     OCCUPATION_DESCRIPTIONS[o.code] = o
+    TITLE_TO_CODE[o.title] = o.code
 
-OCCUPATION_CORE_COMPETENCIES: dict[int, list[CoreCompetency]] = dict()
 for r in csvrows('data/core_competencies.csv'):
     code = int(r['ANZSCO_Code'])
-    if not r['ANZSCO_Title'] == OCCUPATION_DESCRIPTIONS[int(code)].title:
-        print(r)
-        print(OCCUPATION_DESCRIPTIONS[int(code)].title)
-
-    # assert r['ANZSCO_Title'] == OCCUPATION_DESCRIPTIONS[int(code)].title
 
     if code not in OCCUPATION_CORE_COMPETENCIES:
         OCCUPATION_CORE_COMPETENCIES[code] = []
 
     OCCUPATION_CORE_COMPETENCIES[code].append(
             CoreCompetency(r['Core_Competencies'], r['Score']))
+
+for r in csvrows('data/technology_tools.csv'):
+    code = int(r['ANZSCO_Code'])
+    if code not in OCCUPATION_TECHNOLOGY_TOOLS:
+        OCCUPATION_TECHNOLOGY_TOOLS[code] = []
+    OCCUPATION_TECHNOLOGY_TOOLS[code].append(r['Technology_tool'])
